5620dc4aff740136615b64733dd0daa496be6c26
jihaneCameleon <jihane.cameleon@gmail.com>
2022-08-29 17:55:32 +0100

    configuring react-bootstrap and json server

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Navbar} from "react-bootstrap";
+
+export default function Header() {
+  return (
+        <Navbar bg="primary" variant="dark">
+            <Navbar.Brand>Brand text</Navbar.Brand>
+        </Navbar>
+    )
+}
